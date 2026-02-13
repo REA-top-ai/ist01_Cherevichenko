928813cef323cf87702f72347cf17ac24bb45750
Николай <nikolaicherevichenko@mail.ru>
2026-02-13 17:09:53 +0300

    skebob

---
+++ Синтаксис1_Черевиченко_НА.py
@@ -0,0 +1,27 @@
+# 1
+print('Hello, world!')
+
+# 2
+greeting = 'Hello, world!'
+
+# 3
+print(greeting)
+
+
+# 1
+length = 8
+width = 10
+
+print(length * width)
+
+# 2 
+length = 20
+
+print(length * width)
+
+# 3
+length = 23
+width = 13
+
+print(length * width)
+
